7ee1b1e3f70f9aa86143b0051e02a8a4d083d668
Dikijpacan01 <Dikijpacan01@icloud.com>
2024-06-10 13:46:15 +0500

    Update main.go

--- main.go
+++ main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"strconv"
 	"strings"
 )
 
@@ -15,7 +16,9 @@ var rimdigits = map[string]int{
 	"VII":  7,
 	"VIII": 8,
 	"IX":   9,
-	"X":    10,
+	"X":    10, "XI": 11, "XII": 12, "XIII": 13, "XIV": 14, "XV": 15, "XVI": 16, "XVII": 17, "XVIII": 18, "XIX": 19, "XX": 20, "XXI": 21, "XXII": 22, "XXIV": 24,
+	"XXV": 25, "XXVI": 26, "XXIX": 29, "XXX": 30, "XXXI": 31, "XXXV": 35, "XL": 40, "XLI": 41, "XLII": 42, "XLIV": 44, "XLV": 45, "XLVI": 46, "XLIX": 49, "L": 50,
+	"LX": 60, "LXX": 70, "LXXX": 80, "XC": 90, "C": 100, "CD": 400, "D": 500, "DC": 600, "CM": 900, "M": 1000,
 }
 
 var stigidmir = map[int]string{
@@ -28,7 +31,10 @@ var stigidmir = map[int]string{
 	7:  "VII",
 	8:  "VIII",
 	9:  "IX",
-	10: "X", 40: "XL", 50: "L", 90: "XC", 100: "C", 400: "CD", 500: "D", 600: "DC", 900: "CM", 1000: "M",
+	10: "X", 11: "XI", 12: "XII", 13: "XIII", 14: "XIV", 15: "XV", 16: "XVI", 17: "XVII", 18: "XVIII", 19: "XIX", 20: "XX", 21: "XXI", 22: "XXII", 23: "XXIII",
+	24: "XXIV", 25: "XXV", 26: "XXVI", 27: "XXVII", 28: "XXVIII", 29: "XXIX", 30: "XXX", 32: "XXXII", 35: "XXXV", 36: "XXXVI", 40: "XL", 42: "XLII", 45: "XLV",
+	48: "XLVIII", 49: "XLIX", 50: "L", 54: "LIV", 56: "LVI", 60: "LX", 63: "LXIII", 64: "LXIV", 70: "LXX", 72: "LXXII", 80: "LXXX", 81: "LXXXI", 90: "XC", 100: "C",
+	400: "CD", 500: "D", 600: "DC", 900: "CM", 1000: "M",
 }
 
 func rimdig(s string) bool {
@@ -42,6 +48,7 @@ func rimdig(s string) bool {
 }
 
 var arabdigits = map[string]int{
+	"0":  0,
 	"1":  1,
 	"2":  2,
 	"3":  3,
@@ -55,10 +62,13 @@ var arabdigits = map[string]int{
 }
 
 func arabdig(s string) bool {
+	/*if arabdigits[s] == 0 {
+	return false*/
 	for _, c := range s {
 		if _, ok := arabdigits[string(c)]; !ok {
 			return false
 		}
+
 	}
 	return true
 }
@@ -68,46 +78,48 @@ func main() {
 	var operator string
 	var operand1 string
 	var operand2 string
+	var trash string
 	var result int
 
 	fmt.Println("Введите выражение: ")
-	fmt.Scanln(&operand1, &operator, &operand2)
+	fmt.Scanln(&operand1, &operator, &operand2, &trash)
 
 	for rimdig(operand1) {
-		if rimdig(operand2) {
+		if rimdig(operand1) && rimdig(operand2) {
 			operand1 = strings.TrimSpace(operand1)
-			//operator1, _ := strconv.Atoi(operand1)
-
 			operand2 = strings.TrimSpace(operand2)
-			//operator2, _ := strconv.Atoi(operand2)
-			switch operator {
-			case "+":
-				result = rimdigits[operand1] + rimdigits[operand2]
-				fmt.Println(stigidmir[result])
 
-			case "-":
-				result = rimdigits[operand1] - rimdigits[operand2]
-				if result > 0 {
+			if rimdigits[operand1] <= 10 && rimdigits[operand2] <= 10 && len(operand1) <= 4 && len(operand2) <= 4 && len(trash) < 1 {
+				switch operator {
+				case "+":
+					result = rimdigits[operand1] + rimdigits[operand2]
 					fmt.Println(stigidmir[result])
-				} else {
-					panic("В римской системе счисления нет отрицательных чисел")
-				}
 
-			case "*":
-				result = rimdigits[operand1] * rimdigits[operand2]
-				fmt.Println(stigidmir[result])
+				case "-":
+					result = rimdigits[operand1] - rimdigits[operand2]
+					if result > 0 {
+						fmt.Println(stigidmir[result])
+					} else {
+						panic("В римской системе счисления нет отрицательных чисел")
+					}
 
-			case "/":
-				result = rimdigits[operand1] / rimdigits[operand2]
-				if result > 1 {
+				case "*":
+					result = rimdigits[operand1] * rimdigits[operand2]
 					fmt.Println(stigidmir[result])
-				} else {
-					panic("Результатом деления является только целое число")
+
+				case "/":
+					result = rimdigits[operand1] / rimdigits[operand2]
+					if result >= 1 {
+						fmt.Println(stigidmir[result])
+					} else {
+						panic("Результатом деления является только целое число")
+					}
+
+				default:
+					panic("Строка не является математической операцией!!!")
 				}
-
-			default:
-				fmt.Println("Строка не является математической операцией!!!")
-
+			} else {
+				panic("Калькулятор принимает на ввод только 2 числа от I до X включительно")
 			}
 		} else {
 			panic("Используются 2 разные системы счисления")
@@ -115,37 +127,42 @@ func main() {
 		break
 	}
 
-	for arabdig(operand1) {
-		if arabdig(operand2) {
+	for arabdig(operand2) {
+		if arabdig(operand1) && arabdig(operand2) {
+
 			operand1 = strings.TrimSpace(operand1)
-			//operator1, _ := strconv.Atoi(operand1)
-
+			operator1, _ := strconv.Atoi(operand1)
 			operand2 = strings.TrimSpace(operand2)
-			//operator2, _ := strconv.Atoi(operand2)
-			switch operator {
-			case "+":
-				result = arabdigits[operand1] + arabdigits[operand2]
-				fmt.Println(result)
+			operator2, _ := strconv.Atoi(operand2)
+			if operator1 <= 10 && operator2 <= 10 /*&& operator1 != 0 && operator2 != 0*/ && len(trash) < 1 {
 
-			case "-":
-				result = arabdigits[operand1] - arabdigits[operand2]
-				fmt.Println(result)
+				switch operator {
+				case "+":
+					result = arabdigits[operand1] + arabdigits[operand2]
+					fmt.Println(result)
 
-			case "*":
-				result = arabdigits[operand1] * arabdigits[operand2]
-				fmt.Println(result)
+				case "-":
+					result = arabdigits[operand1] - arabdigits[operand2]
+					fmt.Println(result)
 
-			case "/":
-				result = arabdigits[operand1] / arabdigits[operand2]
-				fmt.Println(result)
+				case "*":
+					result = arabdigits[operand1] * arabdigits[operand2]
+					fmt.Println(result)
 
-			default:
-				fmt.Println("Строка не является математической операцией!!!")
+				case "/":
+					result = arabdigits[operand1] / arabdigits[operand2]
+					fmt.Println(result)
+
+				default:
+					panic("Строка не является математической операцией!!!")
+				}
+			} else {
+				panic("Калькулятор принимает на ввод только 2 числа от 1 до 10 включительно")
 			}
 		} else {
 			panic("Используются 2 разные системы счисления")
 		}
 		break
-
 	}
+
 }
